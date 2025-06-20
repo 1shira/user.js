2ab30b1d7126a138292318932b9e5c1a800403af
shira <98327283+1shira@users.noreply.github.com>
2025-06-20 22:37:23 +0200

    Update user.js

--- user.js
+++ user.js
@@ -1107,7 +1107,7 @@ user_pref("_user.js.parrot", "7000 syntax error: the parrot's pushing up daisies
 /* 7016: customize ETP settings
  * [NOTE] FPP (fingerprintingProtection) is ignored when RFP (4501) is enabled
  * [WHY] Arkenfox only supports strict (2701) which sets these at runtime ***/
-   // user_pref("network.cookie.cookieBehavior", 5); // [DEFAULT: 5]
+   user_pref("network.cookie.cookieBehavior", 3); // [DEFAULT: 5]
    // user_pref("network.cookie.cookieBehavior.optInPartitioning", true); // [ETP FF132+]
    // user_pref("network.http.referer.disallowCrossSiteRelaxingDefault", true);
    // user_pref("network.http.referer.disallowCrossSiteRelaxingDefault.top_navigation", true); // [FF100+]
